4352531d2b316d7fd94eda4f033982d7b3011ee9
Egor <20607279+Klef99@users.noreply.github.com>
2022-04-12 14:46:30 +0900

    Add pages

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from flask import Flask, request, render_template
+from run import get_predict
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index.html')
 def index():
     return render_template('index.html')
+
+
+@app.route('/sign_in_page.html')
+def sing_in_page():
+    return render_template('sign_in_page.html')
+
+
+@app.route('/sign_up_page.html')
+def sing_up_page():
+    return render_template('sign_up_page.html')
+
+
+@app.route('/my_recognitions_page.html')
+def my_recognitions_page():
+    return render_template('my_recognitions_page.html')
 
 
 if __name__ == '__main__':
